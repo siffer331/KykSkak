bc0b1f649792390bfad2955777f0f3440f797df0
Tyfl0n <71282715+Tyfl0n@users.noreply.github.com>
2020-09-14 20:27:05 +0200

    Kalde disable rule

--- Main.gd
+++ Main.gd
@@ -3,6 +3,8 @@ extends Control
 onready var in_play: Node = $Margin/DragSplit/DragSplit/InPlay
 onready var rules: Node = $Margin/DragSplit/Rules
 
+var current_rule : Dictionary
+
 func _ready():
 	_new_rule()
 
@@ -11,8 +13,11 @@ func _on_Timer_timeout():
 	_new_rule()
 
 func _new_rule():
-	var current_rule : Dictionary = {name = "regel navn"}#(midlertidig dictiornary. Regler skal komme fra get_rule funktionen i rules noden)rules.get_rule()
-	$ConfirmationDialog/Text.text = "Vil du acceptere reglen:\n" + current_rule.name
-	$ConfirmationDialog.popup()
+	current_rule = {name = "regel navn"}#(midlertidig dictiornary. Regler skal komme fra get_rule funktionen i rules noden)rules.get_rule()
+	$AcceptRule/Text.text = "Vil du acceptere reglen:\n" + current_rule.name
+	$AcceptRule.popup()
 
 
+func _on_AcceptRule_confirmed():
+	#rules.disable_rule(current_rule.id)
+	pass
