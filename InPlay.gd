6d969066c6e68248ee2a22a66d7e46e0322ae9d7
Siffer <siffer@pop-os.localdomain>
2020-09-18 13:14:20 +0200

    Fixes

--- InPlay.gd
+++ InPlay.gd
@@ -11,15 +11,16 @@ func add(rule: String, description: String, id: String) -> void:
 	label.autowrap = true
 	label.hint_tooltip = description
 	label.mouse_filter = MOUSE_FILTER_STOP
-	$Margin/List.add_child(label)
+	$Margin/Scroll/List.add_child(label)
 
 
 func reset() -> void:
-	for id in rules:
-		remove(id)
+	rules = []
+	for child in $Margin/Scroll/List.get_children():
+		child.queue_free()
 
 
 func remove(id: String) -> void:
 	if id in rules:
 		rules.remove(rules.find(id))
-		$Margin/List.get_node(id).queue_free()
+		$Margin/Scroll/List.get_node(id).queue_free()
